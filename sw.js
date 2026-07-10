115dd4b42aa1a019df21a9498a051776605e0753
pasarinobuhito0409-droid <pasarinobuhito0409@gmail.com>
2026-07-10 09:05:29 +0900

    v2: unified FORGE standard (drills24/RQ/map/splash/hero/PASSPORT/electron-view/field-photos + rust-map/confidence-bet/DAILY/teach/gacha/XP)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* VOLT FORGE service worker v1 */
-const CACHE = "volt-forge-v1";
+/* VOLT FORGE service worker v2 */
+const CACHE = "volt-forge-cache-v2";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
